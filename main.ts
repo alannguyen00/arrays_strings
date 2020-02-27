f1fe26d0d5fb54821af448cc30f63814c9637c79
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2020-02-27 13:26:57 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+/**
+ * Write out a list of your five favorite foods here:
+ */
+/**
+ * 1. pho
+ */
+let food1 = "pho"
+// 2. steak
+let food2 = "steak"
+// 3. fried chicken
+let food3 = "fried chicken"
+// 4. ribs
+let food4 = "ribs"
+// 5. sushi
+let food5 = "sushi"
+let listFoods = ["pho", "steak", "fried chicken", "ribs", "sushi"]
+game.splash("This is my first favorite food:" + listFoods[0])
+game.splash("This is my third favorite food:" + listFoods[2])
+game.splash("This is my fifth favorite food:" + listFoods[4])
